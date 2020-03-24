90657cac07657472d4367fe992aa92e6d11aca62
Beatrice Jin <beatricezjin@gmail.com>
2020-03-24 14:05:14 -0400

    NEVER WORK WITH DATES

--- src/components/index/utils/processData.js
+++ src/components/index/utils/processData.js
@@ -1,31 +1,53 @@
 import data from 'Data/data.json';
 import { timeFormat } from 'd3';
 
-function dateString(date) {
-  return timeFormat('%m/%d/%y')(date).substring(1);
+function dateString(date, zero) {
+  const formattedDate = timeFormat('%m/%d/%y')(date).split('/');
+  const mo = zero ? formattedDate[0] : +formattedDate[0];
+  const day = zero ? formattedDate[1] : +formattedDate[1];
+  const yr = formattedDate[2];
+  // console.log(formattedDate)
+  return `${mo}/${day}/${yr}`;
 }
 
 export function processData() {
   console.log(data);
-  const today = dateString(new Date());
-  //const firstDay = new Date('01/22/2020');
+  const dataToUse = [];
+  const today = dateString(new Date(), true);
+  // const firstDay = new Date('01/22/2020');
 
   data.forEach(d => {
-    d.dates = [];
+    const dates = [];
     const firstDay = new Date('01/22/2020');
-    for (let i = firstDay; dateString(i) < today; i = new Date(i.setDate(i.getDate() + 1))){
-      //console.log(d, i, d[i])
-      d.dates.push({
-        date: i,
-        dateString: dateString(i),
-        value: d[dateString(i)]
-      })
+    let j = 0;
+    for (let i = firstDay; dateString(i, true) < today; i = new Date(i.setDate(i.getDate() + 3))){
+      if (j > 2){
+        const value = d[dateString(i, false)];
+        const prev = new Date(i.setDate(i.getDate() - 1));
+        const prev2 = new Date(i.setDate(i.getDate() - 1));
+
+        const prevVal = d[dateString(prev, false)];
+        const prev2Val = d[dateString(prev2, false)];
+        //console.log(value, dateString(prev, false), dateString(prev2, false))
+        dates.push({
+          date: i,
+          dateString: dateString(i, true),
+          value: value,
+          velocity: value - prevVal,
+          acc: (value - prevVal) - (prevVal - prev2Val)
+        })
+      }
+      j++;
     }
+
+    dataToUse.push({
+      country: d['Country/Region'],
+      state: d['Province/State'],
+      dates: dates,
+    })
   })
 
+  console.log('data to use:', dataToUse);
 
-  console.log('data to use:', data);
-
-
-  return data;
+  return dataToUse;
 }
