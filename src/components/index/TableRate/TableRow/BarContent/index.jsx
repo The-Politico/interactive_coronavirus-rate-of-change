c1f04f849d783292ae74aac156fffc7de24c6329
Andrew McGill <arm5077@gmail.com>
2020-03-26 15:58:29 -0400

    Add counter powered by state

--- src/components/index/TableRate/TableRow/BarContent/index.jsx
+++ src/components/index/TableRate/TableRow/BarContent/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styles from './styles.scss';
 class BarContent extends React.Component {
   render () {
-    const { d } = this.props;
-    const sec = (24 * 3600) / d.people;
+    const { people } = this.props;
+    const sec = (24 * 3600) / people;
     return (
       <div className={styles.component + ' class-name'}>
         <div
