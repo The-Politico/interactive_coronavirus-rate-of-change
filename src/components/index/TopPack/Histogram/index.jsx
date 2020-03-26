3c1eefbb5672a6fde0ee5b30092454de309bd3a5
Beatrice Jin <beatricezjin@gmail.com>
2020-03-26 13:59:09 -0400

    just in case

---
+++ src/components/index/TopPack/Histogram/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './styles.scss';
+import { max } from 'd3';
+
+class Histogram extends React.Component {
+  render () {
+    const { data } = this.props;
+    const w = 100 / data.length;
+    const dmax = max(data.map(a => a.new));
+    return (
+      <div className={styles.component + ' class-name'}>
+       {data.map((a, i) =>
+         <div
+          className='bar-container'
+          key={`key-${i}`}
+          style={{ width: `${w}%`}}
+        >
+          <div
+            className={`bar ${a.pastPeak}`}
+            style={{
+              height: `${100 * a.new / dmax}%`
+            }}
+          />
+         </div>
+       )}
+      </div>
+    );
+  }
+}
+export default Histogram;
